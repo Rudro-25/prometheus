5e7120e21cba9d5d5dd705b3ff6e4c06ccb794b5
Rudro-25 <rudro@appscode.com>
2024-06-28 18:27:04 +0600

    prometheus

---
+++ README.md
@@ -0,0 +1,8 @@
+# prometheus
+
+`go run main.go start`
+
+`localhost:8081/ping`
+
+`localhost:8081/metrics` [it will update according to the no of times you refresh -> localhost:8081/ping]
+
---
+++ cmd/start.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/Rudro-25/prometheus/api"
+	"github.com/spf13/cobra"
+)
+
+var Port string
+
+var (
+	startCmd = &cobra.Command{
+		Use:   "start",
+		Short: "Start the server",
+		Long:  `Start the server`,
+		Run: func(cmd *cobra.Command, args []string) {
+			api.RunServer(Port)
+		},
+	}
+)
+
+func init() {
+	fmt.Println("Inside start.go")
+	startCmd.Flags().StringVarP(&Port, "port", "p", "8081", "Port to listen on")
+	rootCmd.AddCommand(startCmd)
+	println("Port: ", Port)
+}
